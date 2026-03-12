01c609d3c69fec9cf70548346500ece0099c4c31
candra kurnia <candra9508@gmail.com>
2026-03-12 13:24:27 +0700

    fix : tittle name and icon website

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import favicon from './assets/ic_logo.png'
+
+const ensureFavicon = (href) => {
+  let link = document.querySelector('link[rel="icon"]')
+  if (!link) {
+    link = document.createElement('link')
+    link.rel = 'icon'
+    document.head.appendChild(link)
+  }
+  link.href = href
+}
+
+ensureFavicon(favicon)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
